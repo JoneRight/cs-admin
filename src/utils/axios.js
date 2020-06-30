1b5ccac379c6e626e0db212b213989952641f3f8
zhangxiangjun@haier.com <zhangxiangjun@haier.com>
2020-06-30 11:14:59 +0800

    feat：上传框架

---
+++ src/utils/axios.js
@@ -0,0 +1,57 @@
+import axios from 'axios'
+import { Message } from 'element-ui'
+import store from '@/store'
+import { getToken } from '@/utils/auth'
+
+// create an axios instance
+const service = axios.create({
+  baseURL: process.env.BASE_API, // api 的 base_url
+  timeout: 5000 // request timeout
+})
+
+// request interceptor
+service.interceptors.request.use(
+  config => {
+    // Do something before request is sent
+    if (store.getters.token) {
+      // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
+      config.headers['X-Token'] = getToken()
+    }
+    return config
+  },
+  error => {
+    // Do something with request error
+    console.log(error) // for debug
+    Promise.reject(error)
+  }
+)
+
+// response interceptor
+service.interceptors.response.use(
+  response => {
+    const res = response.data
+    // 用户不存在的拦截
+    if (res.code === '1001002') {
+      console.log('这里换成跳转你们的登录地址')
+    } else {
+      if (res.isSuccess === false) {
+        // res 回包失败时包装成一个Error对象
+        const err = new Error(res.msg)
+        err.res = res
+        return Promise.reject(err)
+      }
+    }
+    return res
+  },
+  error => {
+    console.log('err' + error) // for debug
+    Message({
+      message: error.message,
+      type: 'error',
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
+
+export default service
